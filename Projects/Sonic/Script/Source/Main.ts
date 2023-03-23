021344db26eeb65bf6d4864b9c85e9a456bf7c11
Avel <avel.lemeemoret@gmail.com>
2023-03-23 17:01:22 +0100

    Sonic movement

--- Projects/Sonic/Script/Source/Main.ts
+++ Projects/Sonic/Script/Source/Main.ts
@@ -5,16 +5,30 @@ namespace Script {
   let viewport: ƒ.Viewport;
   document.addEventListener("interactiveViewportStarted", <EventListener>start);
 
+  let sonic: ƒ.Node
+
   function start(_event: CustomEvent): void {
     viewport = _event.detail;
 
+    sonic = viewport.getBranch().getChildrenByName('Sonic')[0];
+    // let cmpSonic: ƒ.ComponentTransform = sonic.getComponent(ƒ.ComponentTransform);
+    // cmpSonic.mtxLocal.translateX(1);
+
     ƒ.Loop.addEventListener(ƒ.EVENT.LOOP_FRAME, update);
-    // ƒ.Loop.start();  // start the game loop to continously draw the viewport, update the audiosystem and drive the physics i/a
+    ƒ.Loop.start();  // start the game loop to continously draw the viewport, update the audiosystem and drive the physics i/a
   }
 
   function update(_event: Event): void {
     // ƒ.Physics.simulate();  // if physics is included and used
     viewport.draw();
     ƒ.AudioManager.default.update();
+
+    // Make sonic move
+    let sonicPos = sonic.mtxLocal.get()[12];
+    if (sonicPos < 9) {
+      sonic.mtxLocal.translateX(0.1);
+    } else {
+      sonic.mtxLocal.translateX(-9);
+    }
   }
 }
